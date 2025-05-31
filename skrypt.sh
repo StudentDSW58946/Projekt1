4d404bba5435ad00b93d1d48e0ec92f148fb2c06
Daniel <dsw58946@student.dsw.edu.pl>
2025-05-31 18:47:32 +0200

    skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -4,4 +4,13 @@ case $1 in
   --date)
     echo "Dzisiejsza data: $(date)"
     ;;
+  --logs)
+    num=${2:-100}
+    for i in $(seq 1 $num); do
+      filename="log${i}.txt"
+      echo "Nazwa pliku: $filename" > $filename
+      echo "Skrypt: $0" >> $filename
+      echo "Data: $(date)" >> $filename
+    done
+    ;;
 esac
